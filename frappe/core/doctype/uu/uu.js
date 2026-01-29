99c2972a802bdfba3eb705c0e72c5a31b2ad8f42
Mohamed Jamal Sharaf <254368592+mohamed-jamal-sharaf@users.noreply.github.com>
2026-01-29 17:04:36 +0200

    Update uu.js

--- frappe/core/doctype/uu/uu.js
+++ frappe/core/doctype/uu/uu.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2026, Forty Technologies and contributors
 // For license information, please see license.txt
 // mohamed sharaf
+// Hello
 // frappe.ui.form.on("uu", {
 // 	refresh(frm) {
 
