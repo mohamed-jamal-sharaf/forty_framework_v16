0c67dd3b7479fe0c80c2c0ccb8766c2ba53c0412
Mohamed Jamal Sharaf <254368592+mohamed-jamal-sharaf@users.noreply.github.com>
2026-01-29 17:00:38 +0200

    Remove commented-out code and author note

--- frappe/core/doctype/uu/uu.js
+++ frappe/core/doctype/uu/uu.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2026, Forty Technologies and contributors
 // For license information, please see license.txt
-
+// mohamed sharaf
 // frappe.ui.form.on("uu", {
 // 	refresh(frm) {
 
