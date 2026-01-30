b63b531ad1bea5819e0f3252b26d088529eaa907
mohamed-jamal-sharaf <mohamed.jamal.sharaf@icloud.com>
2026-01-30 14:36:19 +0200

    Auto [frappe]: 2026-01-30 14:36:19 - frappe/core/doctype/hjfgdfhjklkhgfdjklk/__init__.py frappe/core/doctype/hjfgdfhjklkhgfdjklk/hjfgdfhjklkhgfdjklk.js frappe/core/doctype/hjfgdfhjklkhgfdjklk/hjfgdfhjklkhgfdjklk.json frappe/core/doctype/hjfgdfhjklkhgfdjklk/hjfgdfhjklkhgfdjklk.py frappe/core/doctype/hjfgdfhjklkhgfdjklk/test_hjfgdfhjklkhgfdjklk.py

--- frappe/core/doctype/hjfgdfhjklkhgfdjklk/hjfgdfhjklkhgfdjklk.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Forty Technologies and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("hjfgdfhjklkhgfdjklk", {
-// 	refresh(frm) {
-
-// 	},
-// });
