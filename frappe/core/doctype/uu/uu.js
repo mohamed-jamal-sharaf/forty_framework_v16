73beaa74f3272d35f8da555f39837edbef1fdaaa
mohamed-jamal-sharaf <mohamed.jamal.sharaf@icloud.com>
2026-01-30 11:49:19 +0200

    Auto: 2026-01-30 11:49:18 — frappe/core/doctype/uu/__init__.py frappe/core/doctype/uu/test_uu.py frappe/core/doctype/uu/uu.js frappe/core/doctype/uu/uu.json frappe/core/doctype/uu/uu.py

--- frappe/core/doctype/uu/uu.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2026, Forty Technologies and contributors
-// For license information, please see license.txt
-// mohamed sharaf
-// Hello
-// frappe.ui.form.on("uu", {
-// 	refresh(frm) {
-
-// 	},
-// });
