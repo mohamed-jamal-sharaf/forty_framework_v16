066dfbd3b320fe089408eaf688b6c39e7a833ffe
mohamed-jamal-sharaf <mohamed.jamal.sharaf@icloud.com>
2026-01-30 12:10:31 +0200

    Auto: 2026-01-30 12:10:31 — frappe/core/doctype/sdhgjkhb/

---
+++ frappe/core/doctype/sdhgjkhb/sdhgjkhb.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Forty Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("sdhgjkhb", {
+// 	refresh(frm) {
+
+// 	},
+// });
